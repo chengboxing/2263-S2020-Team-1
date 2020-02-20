b92a8f302aa325900b3d0e22f26bd60c2544e3f0
epidermus <thelymanboy@gmail.com>
2020-02-20 15:18:29 -0700

    Started getting methods fleshed out and writing notes about what changes we may wish to make.

--- src/main/java/acquire/Board.java
+++ src/main/java/acquire/Board.java
@@ -1,4 +1,25 @@
 package acquire;
 
 public class Board {
+    Tile[][] pieces = //do we want to build a factory class to make these?
+            ; //is here because the comment forced it to be on a separate line
+    Chain[] activeChains = new Chain[6];
+
+
+    public boolean canPlayTile(Tile toPlay){
+        return false;
+    }
+
+    public void placeTile(Tile toPlay){
+
+    }
+
+    //placeTile will call this if the placed tile would cause a merge
+    private void mergeChains(){
+
+    }
+
+    public boolean canEnd(){
+        //checks if end conditions are met
+    }
 }
--- src/main/java/acquire/MergeHandler.java
+++ src/main/java/acquire/MergeHandler.java
@@ -1,4 +1,47 @@
 package acquire;
 
 public class MergeHandler {
+
+    //is this class going to interact with the players directly
+    //or is it going to pass the info to the board, which
+    //in turn sends it to the players?
+
+    //do we want the chain to hold which players have stock in them
+    //to make this easier?
+
+    //we will likely need to pass the players who own stock in the chain to here
+    public void mergeChains(Player[] players){
+
+        //will call all the private methods to determine how to merge
+
+    }
+
+    //checks the Players count of stocks to search for ties
+    private void determineTies(Player[] players){
+
+    }
+
+    //If there is no tie for the largest, Find the largest
+    private Player findLargestShareholder(Player[] players){
+        return null;
+        //returns whoever has the highest share value
+    }
+
+    //If there is no tie for second, finds the Second largest
+    private Player findSecondShareholder(Player[] players){
+        return null;
+        //returns whoever has the second highest value
+    }
+
+    //distributes bonus based on ties. We will probably need to pass
+    //some value to it besides bonus to tell it which
+    //kind of tie it is.
+    private void distributeTieBonus(Player[] players){
+
+    }
+
+    //divides the bonus based on the above functions.
+    private void giveBonuses(){
+        // do we want this to return an int?
+    }
 }
