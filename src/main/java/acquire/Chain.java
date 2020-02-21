9485255bd43d602cee3512bfa607549bb61009b0
aamijani <55214847+aamijani@users.noreply.github.com>
2020-02-21 15:03:58 -0700

    Update Chain.java

--- src/main/java/acquire/Chain.java
+++ src/main/java/acquire/Chain.java
@@ -5,10 +5,12 @@ public class Chain {
     int tilesInChain;
     int stocksOwned;
 
+    //This method checks to see if stocks are available under a certain chain and can be sold to the player.
     private boolean canSellStock(){
         return false;
     }
-
+    
+    //This method merges two or more hotel chains into the one that is bigger. 
     public void merge(){
 
     }
