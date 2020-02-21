f89737f546eada783a1294e1b4371100dd01945e
Chengbo <46608044+chengboxing@users.noreply.github.com>
2020-02-20 22:32:24 -0700

    Update Player.java

--- src/main/java/acquire/Player.java
+++ src/main/java/acquire/Player.java
@@ -1,4 +1,47 @@
 package acquire;
 
 public class Player {
+  
+  private int ID;
+  private int money;
+  private int tile;
+  private int stock;
+  private int share;
+  
+  
+  public Player(int ID){
+    
+    this.ID = ID;
+    this.money = 6000; //$6000 is the starting cash
+    }
+  
+  public int getID() {
+    
+        return ID;
+    }
+  
+  public int getMoney() {
+    
+        return money;
+    }
+  
+  public int holdShare() {
+    
+        return share;
+    }
+  
+   public int buyShare() {
+    
+        return share;
+    }
+  
+  public int tradeShare() {
+    
+        return share;
+    }
+  
+  public int sellShare() {
+    
+        return share;
+    }
 }
