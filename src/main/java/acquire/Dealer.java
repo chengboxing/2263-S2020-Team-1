b0f3b8511f03c381883107773c6cd5e5b55d9897
Janita Aamir <aamijani@isu.edu>
2020-02-21 14:29:42 -0700

    Added changed files to Acquire

--- src/main/java/acquire/Dealer.java
+++ src/main/java/acquire/Dealer.java
@@ -1,4 +1,22 @@
 package acquire;
 
+import java.util.LinkedList;
+
 public class Dealer {
+    Tile[][] pile;
+    LinkedList playerOrder = new LinkedList(); //Needs to be changed to CircularlyLinkedList
+
+
+    public void dealTile(){
+
+    }
+
+    public void removeFromPile(){
+
+    }
+
+    public void moveTurn(){
+
+    }
+
 }
