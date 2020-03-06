bf45b6396ebf6802e502727c8b2b30ec20401284
epidermus <thelymanboy@gmail.com>
2020-03-06 01:29:48 -0700

    Moved the Tile Functionality from Driver to Tile, and started the flyweight design.

--- src/main/java/acquire/Tile.java
+++ src/main/java/acquire/Tile.java
@@ -2,14 +2,24 @@ package acquire;
 
 public class Tile {
     //Location X and Y initialized for the location on the Board
-    private int locationX;
-    private int locationY;
-    //private TileType tiletype;
+    private TileFactory tf = TileFactory.getTileFactory();
+    private String locationX;
+    private String locationY;
+
+    private TileType tiletype;
 
     //Constructor
-    private Tile(Character x, Character y){
-        this.locationX = x;
-        this.locationY = y;
+    public Tile(String x){
+        this.locationX = String.valueOf(x.charAt(0));
+        this.locationY = String.valueOf(x.subSequence(1, x.length()));
+    }
+
+    public String getLocation(){
+        return this.locationX + this.locationY;
+    }
+
+    public Tile getTile(String s){
+        return tf.getTile(s);
     }
 
 }
