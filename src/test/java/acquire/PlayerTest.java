6ec8521ad2a6f673de1f348fa1ff6f72ff103c2f
epidermus <thelymanboy@gmail.com>
2020-04-03 11:34:26 -0600

    added Tests to Board and Player, and added player funcionality to help the tests work

--- src/test/java/acquire/PlayerTest.java
+++ src/test/java/acquire/PlayerTest.java
@@ -6,19 +6,34 @@ import static org.junit.Assert.*;
 public class PlayerTest {
 
     @Test
-    public void playerTestOne(){
-        fail();
+    public void playerIdTest1(){
+        Player p = new Player("p");
+        assertEquals("Didn't create single character name correctly", "p", p.getID());
+    }
+
+    @Test
+    public void playerIdTest2(){
+        Player p = new Player("p l");
+        assertEquals("Didn't handle spaces correctly", "p l", p.getID());
+    }
+
+    @Test
+    public void playerIdTest3(){
+        Player p = new Player("1bh+-=/as{]'\"^%;");
+        assertEquals("Didn't handle random Characters correctly", "1bh+-=/as{]'\"^%;", p.getID());
     }
 
     @Test
     public void negativeNetWorthTest(){
         Player p = new Player("p");
-        p.getWorth();
-        fail();
+        int subtractiion = p.getWorth();
+        p.subtractWorth(subtractiion + 100);
+        assertEquals("Was able to get a negative worth", 0, p.getWorth());
     }
 
     @Test
     public void maxTiles(){
+        Dealer d = new Dealer();
         fail();
     }
 }
